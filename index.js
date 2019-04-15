2d4002d70c7ea0414a711ebc40c41a09f7dd6eb3
Artur Eshenbrener <strate@yandex.ru>
2019-04-15 11:32:58 +0300

    Fixed errors

--- index.js
+++ index.js
@@ -17,27 +17,42 @@ firebaseAdmin.initializeApp({
 });
 
 async function main() {
+    const accountId = 123456;
     // 1. Initialize custom token
     const token = await firebaseAdmin.auth().createCustomToken("4rtL2Ku6NITsJ7GlS6O8NLsgCdQ2", {
         accountIds: [
-            123456
+            accountId
         ]
     });
     // 2. Sign in with custom token
     await firebase.auth().signInWithCustomToken(token);
-    // 3. Try to read collection (actually, collection contains single document)
+    // 3. Try to read collection "chats" (actually, collection contains single document)
     try {
-        await firebase.firestore().collection("chats").get();
-        console.log("SUCCESS: Allow to read collection");
+        await firebase.firestore().collection("chats").where("bot.gcAccount.id", "==", accountId).get();
+        console.log("CHATS1: SUCCESS: Allow to read collection");
     } catch (e) {
-        console.log("ERROR: Can not read collection");
+        console.log("CHATS1: ERROR: Can not read collection:", e.code);
     }
-    // 4. Try to read single document
+    // 4. Try to read single document from "chats"
     try {
         await firebase.firestore().collection("chats").doc("qLk31C5wffSDFnTyahk7").get();
-        console.log("SUCCESS: Allow to read single document");
+        console.log("CHATS1: SUCCESS: Allow to read single document");
     } catch (e) {
-        console.log("ERROR: Can not read single document");
+        console.log("CHATS1: ERROR: Can not read single document");
+    }
+    // 5. Try to read collection "chats2" (actually, collection contains single document)
+    try {
+        await firebase.firestore().collection("chats2").where("gcAccountId", "==", accountId).get();
+        console.log("CHATS2: SUCCESS: Allow to read collection");
+    } catch (e) {
+        console.log("CHATS2: ERROR: Can not read collection:", e.code);
+    }
+    // 6. Try to read single document from "chats2"
+    try {
+        await firebase.firestore().collection("chats2").doc("HBgkG8r88rgwzkPNkX40").get();
+        console.log("CHATS2: SUCCESS: Allow to read single document");
+    } catch (e) {
+        console.log("CHATS2: ERROR: Can not read single document");
     }
 }
 
